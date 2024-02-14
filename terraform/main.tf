709ed2eb6534b0d530e4e35c8af0da8cfb7fdfac
Kopselek <konrad.kkacperek@interia.pl>
2024-02-15 00:06:11 +0100

    IaC with Ansible

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,20 +4,44 @@ resource "vultr_instance" "minecraft" {
   region            = data.vultr_region.warsaw.id
   os_id             = data.vultr_os.ubuntu_22.id
   firewall_group_id = vultr_firewall_group.minecraft.id
-  user_data = join("\n", ["#cloud-config", yamlencode({
-    package_update  = true
-    package_upgrade = true
-    packages = [
-      "openjdk-17-jre-headless",
-      "screen"
+  ssh_key_ids       = [vultr_ssh_key.minecraft.id]
+
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = tls_private_key.tls_key.private_key_pem
+    host        = self.main_ip
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/../ansible/setup-mcserver.yml"
+    destination = "/root/setup-mcserver.yml"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo DEBIAN_FRONTEND=noninteractive apt -y upgrade",
+      "sudo apt-add-repository -y -u ppa:ansible/ansible",
+      "sudo DEBIAN_FRONTEND=noninteractive apt -y install ansible",
+      "sudo ansible-playbook /root/setup-mcserver.yml",
     ]
-    runcmd = [
-      "wget 'https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar' -O minecraft_server.jar",
-      "echo \"eula=true\" > eula.txt",
-      "ufw disable",
-      "screen -dmS minecraft java -Xms1024M -Xmx1024M -jar minecraft_server.jar nogui"
-    ]
-  })])
+  }
+
+  # user_data = join("\n", ["#cloud-config", yamlencode({
+  #   package_update  = true
+  #   package_upgrade = true
+  #   packages = [
+  #     "openjdk-17-jre-headless",
+  #     "screen"
+  #   ]
+  #   runcmd = [
+  #     "wget 'https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar' -O minecraft_server.jar",
+  #     "echo \"eula=true\" > eula.txt",
+  #     "ufw disable",
+  #     "screen -dmS minecraft java -Xms1024M -Xmx1024M -jar minecraft_server.jar nogui"
+  #   ]
+  # })])
 }
 
 resource "vultr_block_storage" "minecraft" {
@@ -29,6 +53,7 @@ resource "vultr_block_storage" "minecraft" {
 }
 
 resource "vultr_firewall_group" "minecraft" {
+  description = "base group"
 }
 
 resource "vultr_firewall_rule" "minecraft" {
@@ -57,4 +82,14 @@ resource "cloudflare_record" "minecraft" {
   ttl     = 60
   value   = vultr_instance.minecraft.main_ip
   proxied = false
+}
+
+resource "vultr_ssh_key" "minecraft" {
+  name    = "vultr-ssh-key"
+  ssh_key = tls_private_key.tls_key.public_key_openssh
+}
+
+resource "tls_private_key" "tls_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,9 @@
 output "minecraft_ip" {
-  value = vultr_instance.minecraft.main_ip
+  description = "Public IP of server"
+  value       = vultr_instance.minecraft.main_ip
 }
 
 output "minecraft_hostname" {
-  value = var.cloudflare ? local.cloudflare_minecraft_hostname : null
+  description = "Public hostname of server"
+  value       = var.cloudflare ? local.cloudflare_minecraft_hostname : null
 }
