94006f120faedac1715ea8281fb6c78e5c23112a
Kopselek <konrad.kkacperek@interia.pl>
2024-02-15 23:40:06 +0100

    remove commented section

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,21 +27,6 @@ resource "vultr_instance" "minecraft" {
       "sudo ansible-playbook /root/setup-mcserver.yml",
     ]
   }
-
-  # user_data = join("\n", ["#cloud-config", yamlencode({
-  #   package_update  = true
-  #   package_upgrade = true
-  #   packages = [
-  #     "openjdk-17-jre-headless",
-  #     "screen"
-  #   ]
-  #   runcmd = [
-  #     "wget 'https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar' -O minecraft_server.jar",
-  #     "echo \"eula=true\" > eula.txt",
-  #     "ufw disable",
-  #     "screen -dmS minecraft java -Xms1024M -Xmx1024M -jar minecraft_server.jar nogui"
-  #   ]
-  # })])
 }
 
 resource "vultr_block_storage" "minecraft" {
